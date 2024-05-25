fce778e7a9e6b154b972aa332527d01ce6100e2a
Булат Хангильдин <khangildinba@gmail.com>
2024-05-25 22:06:55 +0300

    added cmp-nvim-lua

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -6,6 +6,7 @@ return {
     dependencies = {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-path",
       { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
       "onsails/lspkind.nvim",
